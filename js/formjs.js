665092aff85230c9ee7d57c47c622553ad206997
AlexanderM <ad.mihotin@gmail.com>
2018-09-02 01:28:41 +0300

    JS

--- js/formjs.js
+++
@@ -1,8 +0,0 @@
-
-var button = document.querySelector(".search__button");
-var form = document.querySelector(".search-form-block");
-
-button.addEventListener("click", function (evt) {
-evt.preventDefault();
-form.classList.toggle("search-form");
-});
